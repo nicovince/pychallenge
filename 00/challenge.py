35543eee1e90fa5e4af25862dab62900aa6fc4bd
Nicolas VINCENT <nico.vince@gmail.com>
2023-03-07 19:39:31 +0100

    Rename folders.

---
+++ 00/challenge.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+import math
+
+val = int(math.pow(2,38))
+print(f"http://www.pythonchallenge.com/pc/def/{val}.html")
--- 01/challenge.py
+++ 01/challenge.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python3
+import string
 
-import math
+input_str = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
 
-val = int(math.pow(2,38))
-print(f"http://www.pythonchallenge.com/pc/def/{val}.html")
+alphabet = string.ascii_lowercase
+url = "map"
+
+def translate_letters(data, offset):
+    table = f"{alphabet[offset:]}{alphabet[0:offset]}"
+    trans = data.translate(str.maketrans(alphabet, table))
+    print(trans)
+    return trans
+
+translate_letters(input_str, 2)
+next = translate_letters(url, 2)
+print(f"http://www.pythonchallenge.com/pc/def/{next}.html")
